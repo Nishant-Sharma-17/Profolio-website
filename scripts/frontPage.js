12b0da7831a0d22017f48b1371b10fc1dd56b55c
Nishant Sharma <nishant9574@gmail.com>
2025-01-26 18:23:37 +0530

    Dynamically genrated shadows individually for skills using fast-average-color liabrary

--- scripts/frontPage.js
+++ scripts/frontPage.js
@@ -1,12 +1,46 @@
+import {FastAverageColor} from './node_modules/fast-average-color/dist/index.js'
+
 // Add smooth scrolling behavior to navigation links
+
 document.querySelectorAll('.sections-link').forEach((link) => {
+
     link.addEventListener('click',function (event){
+
         event.preventDefault(); // Prevent default anchor behavior
+
         const targetId = this.getAttribute('href'); // Get the target section ID
-        console.log(targetId)
+
+        // console.log(targetId);
+
         const targetElement = document.querySelector(targetId);
+
         // Scroll to the target element smoothly
         targetElement.scrollIntoView({ behavior:'smooth',block:'start',inline:'start'});
     });
 });
-console.log('hello');
+window.addEventListener('load',function(){
+    const fac=new FastAverageColor();
+    document.querySelectorAll('.skills .skill-button').forEach((button)=>{
+        const element =button.querySelector('img');
+       if(element){
+        const color =fac.getColor(element).rgb;
+        // console.log(color.rgb);
+        button.style.boxShadow=`0px 3px 7px ${color}`
+    }
+    else
+    {
+    button.style.boxShadow=`0px 3px 7px antiquewhite`
+       }
+        // console.log(button);
+        // console.log(element)
+        // console.log(fac.getColor(element));
+        // button.style.boxShadow=' 0px 3px 7px blue'
+    });
+    
+    // const color=fac.getColor(document.querySelector('.my-image'));
+    // console.log(color.rgb);
+    // console.log(fac.getColor(document.querySelector('.my-image')).rgb);
+    // const sidebarDiv=document.querySelector('.sidebar-div');
+    // console.log(sidebarDiv);
+    // sidebarDiv.style.backgroundColor=color.rgb;
+});
